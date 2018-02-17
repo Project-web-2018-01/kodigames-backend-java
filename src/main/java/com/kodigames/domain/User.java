1d4fca8b6a9490aca226ce1b4a78d744469225d9
paulina.kuzmicka@gmail.com <paulina.kuzmicka@gmail.com>
2018-02-17 23:23:13 +0100

    User class

---
+++ src/main/java/com/kodigames/domain/User.java
@@ -0,0 +1,36 @@
+package com.kodigames.domain;
+
+public class User {
+    private Long userId;
+    private String name;
+    private String surname;
+    private String login;
+    private String password;
+
+    public User(Long userId, String name, String surname, String login, String password) {
+        this.userId = userId;
+        this.name = name;
+        this.surname = surname;
+        this.login = login;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+}
