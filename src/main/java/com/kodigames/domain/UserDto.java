f57a1a338fda38b2975f21e6dd737bfadcfca6a1
paulina.kuzmicka@gmail.com <paulina.kuzmicka@gmail.com>
2018-02-21 23:44:22 +0100

    UserDto, UserMapper classes added

--- src/main/java/com/kodigames/domain/UserDto.java
+++ src/main/java/com/kodigames/domain/UserDto.java
@@ -1,18 +1,14 @@
 package com.kodigames.domain;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+public class UserDto {
 
-@Entity
-@Table(name = "USERS")
-public class User {
     private Long userId;
     private String name;
     private String surname;
     private String login;
     private String password;
 
-    public User(Long userId, String name, String surname, String login, String password) {
+    public UserDto(Long userId, String name, String surname, String login, String password) {
         this.userId = userId;
         this.name = name;
         this.surname = surname;
@@ -20,37 +16,22 @@ public class User {
         this.password = password;
     }
 
-    public User() {
-    }
-
-    @Id
-    @GeneratedValue
-    @NotNull
-    @Column(name = "ID")
     public Long getUserId() {
         return userId;
     }
 
-    @NotNull
-    @Column(name = "NAME")
     public String getName() {
         return name;
     }
 
-    @NotNull
-    @Column(name = "SURNAME")
     public String getSurname() {
         return surname;
     }
 
-    @NotNull
-    @Column(name = "LOGIN")
     public String getLogin() {
         return login;
     }
 
-    @NotNull
-    @Column(name = "PASSWORD")
     public String getPassword() {
         return password;
     }
