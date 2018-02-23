5f6d5a86379f01239cebac54b6890cf14acaccf6
“Maciolek <maciolek.dariusz@gmail.com>
2018-02-23 17:14:35 +0100

    corrected all bugs - coflicts and found games classes :-)
    corrected PR
    
    add corrected after PR
    
     changed long id to wrapper, added adnotations Hibernet
    
    corrected PR - games
    
     changed long id to wrapper, added adnotations Hibernet
    
    fixed games class and conflicts

---
+++ src/main/java/com/kodigames/domain/Score.java
@@ -0,0 +1,55 @@
+package com.kodigames.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity()
+@Table(name = "SCORES")
+public final class Score {
+
+    private Long id;
+    private int score;
+    private Long userGameId;
+
+    public Score(Long id, int score, Long userGameId) {
+        this.id = id;
+        this.score = score;
+        this.userGameId = userGameId;
+    }
+
+    public Score() {
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @NotNull
+    @Column(name = "SCORE")
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "USER_GAME_ID")
+    public Long getUserGameId() {
+        return userGameId;
+    }
+
+    public void setUserGameId(Long userGameId) {
+        this.userGameId = userGameId;
+    }
+}
---
+++ src/main/java/com/kodigames/domain/ScoreDto.java
@@ -0,0 +1,39 @@
+package com.kodigames.domain;
+
+public final class ScoreDto {
+
+    private Long id;
+    private int score;
+    private Long userGameId;
+
+    public ScoreDto(Long id, int score, Long userGameId) {
+        this.id = id;
+        this.score = score;
+        this.userGameId = userGameId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Long getUserGameId() {
+        return userGameId;
+    }
+
+    public void setUserGameId(Long userGameId) {
+        this.userGameId = userGameId;
+    }
+}
+
