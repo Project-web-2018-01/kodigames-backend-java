74a6b0b5b1b432de6a3fe1869dac887a3b610f56
paulina.kuzmicka@gmail.com <paulina.kuzmicka@gmail.com>
2018-02-23 23:10:23 +0100

    userId to id, name to firstName

--- src/main/java/com/kodigames/domain/UserDto.java
+++ src/main/java/com/kodigames/domain/UserDto.java
@@ -1,27 +1,27 @@
 package com.kodigames.domain;
 
-public class UserDto {
+public final class UserDto {
 
-    private Long userId;
-    private String name;
+    private long id;
+    private String firstName;
     private String surname;
     private String login;
     private String password;
 
-    public UserDto(Long userId, String name, String surname, String login, String password) {
-        this.userId = userId;
-        this.name = name;
+    public UserDto(Long id, String firstName, String surname, String login, String password) {
+        this.id = id;
+        this.firstName = firstName;
         this.surname = surname;
         this.login = login;
         this.password = password;
     }
 
-    public Long getUserId() {
-        return userId;
+    public long getId() {
+        return id;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
     public String getSurname() {
@@ -34,5 +34,25 @@ public class UserDto {
 
     public String getPassword() {
         return password;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
--- src/main/java/com/kodigames/mapper/UserMapper.java
+++ src/main/java/com/kodigames/mapper/UserMapper.java
@@ -7,17 +7,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserMapper {
 
-    public UserDto mapToUserDto(final User user){
-        return new UserDto(user.getUserId(),
-                user.getName(),
+    public UserDto mapToUserDto(final User user) {
+        return new UserDto(user.getId(),
+                user.getFirstName(),
                 user.getSurname(),
                 user.getLogin(),
                 user.getPassword());
     }
 
-    public User mapToUser(final UserDto userDto){
-        return new User(userDto.getUserId(),
-                userDto.getName(),
+    public User mapToUser(final UserDto userDto) {
+        return new User(userDto.getId(),
+                userDto.getFirstName(),
                 userDto.getSurname(),
                 userDto.getLogin(),
                 userDto.getPassword());
