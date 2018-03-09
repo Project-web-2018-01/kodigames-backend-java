2a9b670dd608be616306e468f66156a9041463a2
MalczewskiKarol <karolmalczewski2@o2.pl>
2018-03-09 13:28:52 +0100

    first commit

---
+++ src/main/java/com/kodigames/KodigamesApplication.java
@@ -0,0 +1,12 @@
+package com.kodigames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodigamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodigamesApplication.class, args);
+	}
+}
