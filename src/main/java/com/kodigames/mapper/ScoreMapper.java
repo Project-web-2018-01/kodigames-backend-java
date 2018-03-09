e7d42b9374befe8cd1ecbdae05eb36a190e591a0
Dariusz <33531485+Maciolek@users.noreply.github.com>
2018-03-09 22:50:17 +0100

    added - score, scoreDto, scoreMapper (#5)
    * Domain classes + Mapper class (#8)
    
    * Domain classes added - Game/GameDto, GameMapper added
    
    * added - score, scoreDto, scoreMapper
    
    *  changed long id to wrapper, added adnotations Hibernet

---
+++ src/main/java/com/kodigames/mapper/ScoreMapper.java
@@ -0,0 +1,23 @@
+package com.kodigames.mapper;
+
+import com.kodigames.domain.Score;
+import com.kodigames.domain.ScoreDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScoreMapper {
+
+    public ScoreDto mapToScoreDto(final Score score) {
+        return new ScoreDto(
+                score.getId(),
+                score.getScore(),
+                score.getUserGameId());
+    }
+
+    public Score mapToScore(final ScoreDto scoreDto) {
+        return new Score(
+                scoreDto.getId(),
+                scoreDto.getScore(),
+                scoreDto.getUserGameId());
+    }
+}
