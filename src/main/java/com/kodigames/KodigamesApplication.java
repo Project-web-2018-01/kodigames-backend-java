9e09032c203bdf1686f70223e8c8094a53c7e059
“Maciolek <maciolek.dariusz@gmail.com>
2018-03-16 22:46:44 +0100

    fixed gradle

--- src/main/java/com/kodigames/KodigamesApplication.java
+++ src/main/java/com/kodigames/KodigamesApplication.java
@@ -2,11 +2,20 @@ package com.kodigames;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KodigamesApplication {
+public class KodigamesApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KodigamesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KodigamesApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(KodigamesApplication.class);
+    }
 }
+
+
